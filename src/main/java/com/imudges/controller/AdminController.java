88959ca335b46d336a460bb4f9f01f8dedcad692
Zyzzz <zhouyan@imudges.com>
2016-11-14 17:07:19 +0800

    管理员登录界面

---
+++ src/main/java/com/imudges/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.imudges.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2016/11/14.
+ */
+
+
+@Controller
+public class AdminController {
+
+
+}
